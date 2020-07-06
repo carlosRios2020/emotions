5d96c09408bd6c8ac4c342acd328811ca7c252da
cskinner@uniempresarial.edu.co <cskinner@uniempresarial.edu.co>
2020-07-06 18:40:04 -0500

    actualizacion

--- main.js
+++ main.js
@@ -6,14 +6,18 @@ var texto = "";
 var texto  = document.getElementById('texto');
 const btn = document.getElementById('enviar');
 const respuesta = document.getElementById('mostrar-publicacion');
+const btn2 = document.getElementById('enviar-2');
+const respuesta2 = document.getElementById('mostrar-publicacion2');
+var indices1 = 0;
+var indices2 = 0;
 
 //listeners
 cargarEventlistener();
 
 function cargarEventlistener() {
 
-    btn.addEventListener('click', traerTexto);
-
+    btn.addEventListener('click', validarPalabrasPositivas);
+    btn2.addEventListener('click', validadPalabrasNegativas);
 }
 
 //funciones
@@ -21,7 +25,7 @@ function cargarEventlistener() {
 
 var publicacion = texto.value + ',';
 
-function traerTexto() {
+function validarPalabrasPositivas() {
 
     let i,j;
     let salida = [];
@@ -39,11 +43,9 @@ function traerTexto() {
                 if(textToArreglo[i] == listadoEmocionesPositivas[j]){
 
                     salida.push(textoActual);
-                    var indices = salida.length - 1;
-            
-                    console.log(" tenemos " + indices + " elementos iguales");
+                    indices1 = salida.length - 1;
 
-                }else{
+                } else {
                      continue;
                 }
             }
@@ -51,7 +53,51 @@ function traerTexto() {
     }else{
         console.log('no hay mensaje para iterar')
     } 
-    return document.write("la respuesta son "+ salida + "y son " + indices + " elemento de su comentario positivos");
+    return respuesta.innerHTML = "la respuesta son "+ salida + "y son " + indices1 + " elemento de su comentario positivos";
 }
 
+
+function  validadPalabrasNegativas(){
+
+        let i,j;
+        let salida = [];
+       
+        const textToArreglo = texto.value.split(" ");
+        
+        if(textToArreglo.length > 0 || textToArreglo != undefined){
+    
+            for(i = 0; i <= textToArreglo.length; i++){
+    
+                var textoActual = textToArreglo[i];
+    
+                for(j=  0; j <=listadoEmocionesNegativas.length;j++){
+    
+                    if(textToArreglo[i] == listadoEmocionesNegativas[j]){
+    
+                        salida.push(textoActual);
+                        indices2 = salida.length - 1;
+    
+                    } else {
+                         continue;
+                    }
+                }
+           }
+        }else{
+            console.log('no hay mensaje para iterar')
+        } 
+        return respuesta2.innerHTML = "la respuesta son "+ salida + "y son " + indices2 + " elemento de su comentario Negativas" + conteoEmociones(indices1, indices2);
+}
+
+function conteoEmociones(indices1, indices2){
+
+    let publicable = false;
+
+    if(indices1 > indices2 ){
+        publicable = true;
+        alert("su mensaje es positivo y se puede publicar")
+    } else{
+        alert("su mensaje no cumple nuestras politicas ** sea mas positiv@");
+    }
+    return documet.write(`su aprobacion para publicar es ${publicable}`);
+}
 
